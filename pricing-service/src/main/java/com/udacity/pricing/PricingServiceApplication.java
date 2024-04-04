6a5c66655b5f6a1e249829351bf4c8be97c18c84
l3x-fx <kekskiller@email.com>
2024-04-04 15:58:04 +0200

    pricing microservice registration

--- pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -2,12 +2,14 @@ package com.udacity.pricing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * Creates a Spring Boot Application to run the Pricing Service.
  * TODO: Convert the application from a REST API to a microservice.
  */
 @SpringBootApplication
+//@EnableEurekaClient is not required anymore -> https://github.com/spring-cloud/spring-cloud-netflix/issues/4022#issuecomment-915991198
 public class PricingServiceApplication {
 
     public static void main(String[] args) {
